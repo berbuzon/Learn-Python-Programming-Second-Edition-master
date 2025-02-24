1e81f64081750deb3c6a87e0cc0eb700812e7def
Bernardo Eliezer <berbuzon@gmail.com>
2025-02-24 09:01:06 -0300

    cambios trabajo

--- Chapter02/ch2/bytearray.py
+++ Chapter02/ch2/bytearray.py
@@ -1,22 +1,4 @@
 # bytearray.py
-
-
-<<<<<<< HEAD
->>> bytearray()  # empty bytearray object
-bytearray(b'')
->>> bytearray(10)  # zero-filled instance with given length
-bytearray(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
->>> bytearray(range(5))  # bytearray from iterable of integers
-bytearray(b'\x00\x01\x02\x03\x04')
->>> name = bytearray(b'Lina')  #A - bytearray from bytes
->>> name.replace(b'L', b'l')
-bytearray(b'lina')
->>> name.endswith(b'na')
-True
->>> name.upper()
-bytearray(b'LINA')
->>> name.count(b'L')
-=======
 bytearray()  # empty bytearray object
 bytearray(b'')
 bytearray(10)  # zero-filled instance with given length
@@ -31,5 +13,4 @@ True
 name.upper()
 bytearray(b'LINA')
 name.count(b'L')
->>>>>>> e79eee5d791c9eb94aaaa3df35dc2c62331731d4
-1
+
