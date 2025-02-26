5bb2139b668535b68beaf9b9a7d791618a136616
Bernardo Eliezer <berbuzon@gmail.com>
2025-02-26 14:29:21 -0300

    Chapter03

--- Chapter03/ch3/borrame.py
+++ Chapter03/ch3/borrame.py
@@ -1,19 +1,12 @@
-# primos=[]
-# limite=100
+primos=[]
+limite=100
 
-
-# for n in range(2,limite+1):
-#     flag=True
-#     for divisor in range(2,n):
-#         if n % divisor == 0:
-#             flag=False
-#             break
-#     if flag:
-#         primos.append(n)
-# print(primos)
-
-n=10
-
-for divisor in range(2,n):
-    if n % divisor == 0:
-        print(divisor)
+for n in range(2,limite+1):
+    flag = True
+    for divisor in range(2,n):
+        if n % divisor == 0:
+            flag=False
+            break
+    if flag:
+            primos.append(n)
+print (primos)
